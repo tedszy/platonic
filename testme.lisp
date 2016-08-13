c698792a14104cba883c01f164bebc8ae63590ef
Ted Szylowiec <tedszy@gmail.com>
2016-08-13 12:21:06 +0700

    format tests

--- testme.lisp
+++ testme.lisp
@@ -34,11 +34,8 @@
 
 (test vertex/edge/face-hash (hash-vertex/edge/face '(6 2 3 5)) = 2356)
 (let ((id (make-group-element '(1 2 3 4))))
-  (test vertex/edge/face-transform
-	(transform-vertex/edge/face id '(1 2 3)) equalp '(1 2 3)))
+  (test vertex/edge/face-transform (transform-vertex/edge/face id '(1 2 3)) equalp '(1 2 3)))
 
-;; Take advantage of the fact that faces are transformed
-;; just like vertices for a tetrahedron.
 (let ((c (make-tetrahedron-configuration 'faces '(a b c d)))
       (g (make-group-element '(4 3 2 1)))
       (h (make-group-element '(3 1 4 2))))
@@ -51,29 +48,12 @@
 	equalp
 	'(faces ((1 2 3) B) ((1 2 4) D) ((1 3 4) A) ((2 3 4) C))))
 
-(test tetrahedron-2
-      (length (distinct-tetrahedron-colorings '(r g)))   
-      = 
-      (burnside-tetrahedron 2))
-(test tetrahedron-3
-      (length (distinct-tetrahedron-colorings '(r g b)))  
-      = 
-      (burnside-tetrahedron 3))
-(test tetrahedron-4
-      (length (distinct-tetrahedron-colorings '(r g b y)))
-      = 
-      (burnside-tetrahedron 4))
-(test tetrahedron-5
-      (length (distinct-tetrahedron-colorings '(r g b y k)))
-      = 
-      (burnside-tetrahedron 5))
-(test cube-2
-      (length (distinct-cube-colorings '(r g)))
-      = 
-      (burnside-cube 2))
-(test cube-3
-      (length (distinct-cube-colorings '(r g b)))
-      = 
-      (burnside-cube 3))
+(test tetrahedron-2 (length (distinct-tetrahedron-colorings '(r g))) = (burnside-tetrahedron 2))
+(test tetrahedron-3 (length (distinct-tetrahedron-colorings '(r g b))) = (burnside-tetrahedron 3))
+(test tetrahedron-4 (length (distinct-tetrahedron-colorings '(r g b y))) = (burnside-tetrahedron 4))
+(test tetrahedron-5 (length (distinct-tetrahedron-colorings '(r g b y k))) = (burnside-tetrahedron 5))
+
+(test cube-2 (length (distinct-cube-colorings '(r g))) = (burnside-cube 2))
+(test cube-3 (length (distinct-cube-colorings '(r g b))) = (burnside-cube 3))
 
 
